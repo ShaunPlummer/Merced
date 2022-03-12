01113c2baedf97ff6446e2edb533a2892899fa05
shaun <shaunplummer247@aol.com>
2022-03-12 23:09:56 +0000

    feat: add top app bar
    Introduce material 3 dependency and add a top app bar. displaying the app name.

--- app/src/main/java/com/washuTechnologies/merced/ui/components/MercedScaffold.kt
+++ app/src/main/java/com/washuTechnologies/merced/ui/components/MercedScaffold.kt
@@ -1,8 +1,15 @@
 package com.washuTechnologies.merced.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.washuTechnologies.merced.R
 
 /**
  * Main scaffold for use across the app.
@@ -12,6 +19,19 @@ fun MercedScaffold(
     content: @Composable (PaddingValues) -> Unit
 ) {
     Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(
+                        stringResource(id = R.string.app_name),
+                        color = contentColorFor(backgroundColor = MaterialTheme.colors.primary)
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colors.primary
+                )
+            )
+        },
         content = content
     )
 }
