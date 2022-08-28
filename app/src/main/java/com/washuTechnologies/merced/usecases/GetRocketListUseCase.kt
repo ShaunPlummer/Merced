79ec3f801f7b61bcd5776a9689c593405f5d3692
shaun <shaunplummer247@aol.com>
2022-08-28 21:14:50 +0100

    docs: add missing class doc

--- app/src/main/java/com/washuTechnologies/merced/usecases/GetRocketListUseCase.kt
+++ app/src/main/java/com/washuTechnologies/merced/usecases/GetRocketListUseCase.kt
@@ -5,9 +5,15 @@ import com.washuTechnologies.merced.data.launches.model.RocketLaunch
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Use Case to retrieve a list of rocket launches.
+ */
 class GetRocketListUseCase @Inject constructor(
     private val rocketLaunchRepository: RocketLaunchRepository
 ) {
 
+    /**
+     * Returns a flow containing the launch list.
+     */
     operator fun invoke(): Flow<Array<RocketLaunch>> = rocketLaunchRepository.getLaunchList()
 }
