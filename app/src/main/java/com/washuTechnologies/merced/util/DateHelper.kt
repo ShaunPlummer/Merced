132a46a11de0051019c8c8be624ad4e8e787c527
shaun <shaunplummer247@aol.com>
2022-09-04 13:52:45 +0100

    refactor: remove * import

--- app/src/main/java/com/washuTechnologies/merced/util/DateHelper.kt
+++ app/src/main/java/com/washuTechnologies/merced/util/DateHelper.kt
@@ -2,7 +2,8 @@ package com.washuTechnologies.merced.util
 
 import com.washuTechnologies.merced.data.launches.model.RocketLaunch
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 /**
  * Helpers for working with date information about rocket launches.
